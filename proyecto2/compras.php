3218298613c26a85eb33da59823a01770240b32f
carlos <asehca@yahoo.com>
2024-07-01 14:14:52 +0200

    inicio del proyecto de restaurante

---
+++ proyecto2/compras.php
@@ -0,0 +1,76 @@
+<?php include 'log/header.php'?>
+
+<script src="js/funcion.js"></script>
+
+<div class="container">
+
+
+
+<h1 style="text-align:center; margin-top: 100px">COMPRAS DE LA SEMANAS</h1>
+
+<div
+    class="table-responsive"
+>
+    <table
+        class="table table-primary"
+    >
+        <thead>
+            <tr>
+                <th scope="col"></th>
+                <th scope="col">LUNES</th>
+                <th scope="col">MARTES</th>
+                <th scope="col">MIERCOLES</th>
+                <th scope="col">JUEVES</th>
+                <th scope="col">VIERNES</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="" style="height:90px">
+                <td scope="row">PRODUCTOS:</td>
+                <td id="demo1"></td>
+                <td id="demo3"></td>
+                <td id="demo5"></td>
+                <td id="demo7"></td>
+                <td id="demo9"></td>
+                <td></td>
+              
+
+            </tr>
+            <tr class="" style="height:50px">
+                <td scope="row">COSTO:</td>
+                <td id="demo2"></td>
+                <td id="demo4"></td>
+                <td id="demo6"></td>
+                <td id="demo8"></td>
+                <td id="demo10"></td>
+                <td id="demo11"></td>
+
+
+
+
+
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+
+
+<div>
+
+<button type="button" class=" btn btn-danger" onclick="funcion1()">MIS COMPRAS</button>
+
+</div>
+
+<div>
+<p style="padding:20px; color:black; background-color: beige;border: 2px solid black;height:300px;width:450px; float: right" id="demo12"></p>
+</div>
+
+
+
+
+
+
+
+</div>
